ae2bc03bb1c6e5896dc8043a8f841691fe256e74
Robin Weser <contact@weser.io>
2019-08-30 17:58:57 +0200

    remove webpack example

--- examples/webpack/elo.js
+++
@@ -1,9 +0,0 @@
-var generator = require('@elodin/generator-css-in-js').default
-
-module.exports = {
-  generator: generator({
-    adapter: 'react-fela',
-  }),
-  errors: 'throw',
-  plugins: [],
-}
--- examples/webpack/src/run.js
+++
@@ -1,7 +0,0 @@
-var join = require('path').join
-var transformFile = require('@elodin/core').transformFile
-var cssGenerator = require('@elodin/generator-css-in-js').default
-
-console.log(cssGenerator)
-
-transformFile(join(__dirname, 'style.elodin'), cssGenerator)
