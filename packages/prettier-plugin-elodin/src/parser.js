1ed44c36a1b8dde1a5546a1e65d82accf9014c35
Robin Weser <contact@weser.io>
2019-09-06 13:30:36 +0200

    #20 prettier-plugin throw on error

--- packages/prettier-plugin-elodin/src/parser.js
+++ packages/prettier-plugin-elodin/src/parser.js
@@ -1,11 +1,19 @@
 import { format } from '@elodin/format'
 
-export default function parse(text) {
-  const formattedText = format(text).code
+export default function parse(text, _, options) {
+  const formatted = format(text)
+
+  const errors = formatted.errors
+  let formattedText = formatted.code
+
+  if (errors.length > 0) {
+    let formattedText = text
+  }
 
   return {
     ast_type: 'elodin-ast',
     body: formattedText,
+    error: errors[0],
     end: text.length,
     source: text,
     start: 0,
