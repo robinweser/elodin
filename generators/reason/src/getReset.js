1ba01950b380779d4ddece6353fd395bdd16725c
Robin Weser <contact@weser.io>
2019-09-07 00:35:42 +0200

    fix rests in reason package

--- generators/reason/src/getReset.js
+++ generators/reason/src/getReset.js
@@ -1,16 +1,16 @@
-const viewReset = `display:flex;align-self:stretch;flex-direction:column;flex-shrink:0;max-width:100%;box-sizing:border-box`
-const textReset = `display:inline`
+const viewResetStyle = `display:flex;align-self:stretch;flex-direction:column;flex-shrink:0;max-width:100%;box-sizing:border-box`
+const textResetStyle = `display:inline`
 
-const rootReset = `${viewReset};position:fixed;top:0;bottom:0;left:0;right:0`
+const rootResetStyle = `${viewResetStyle};position:fixed;top:0;bottom:0;left:0;right:0`
 
 const defaultGenerateClassName = type => '_elo_' + type
 export function baseReset(generateClassName = defaultGenerateClassName) {
   const viewClassName = generateClassName('view')
   const textClassName = generateClassName('text')
 
-  return `.${viewClassName}{${viewReset}}.${textClassName}{${textReset}}`
+  return `.${viewClassName}{${viewResetStyle}}.${textClassName}{${textResetStyle}}`
 }
 
 export function rootReset(rootNode = 'body') {
-  return `${rootNode}{${rootReset}}`
+  return `${rootNode}{${rootResetStyle}}`
 }
