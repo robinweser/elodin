4444c9923b164139d5d5457cd84142d7a71ddfd4
Robin Weser <contact@weser.io>
2019-09-26 09:30:18 +0200

    fix tests

--- generators/reason/src/__tests__/generate-test.js
+++ generators/reason/src/__tests__/generate-test.js
@@ -3,139 +3,65 @@ import { parse } from '@elodin/core'
 import createGenerator from '../createGenerator'
 
 const file = `
-variant Variant {
+variant Type {
   Primary
-  Destructive
+  Secondary
 }
 
-variant Intent {
-  Inline
-  Outline
-}
-
-variant Size {
-  Small
-  Normal
-  Big
+variant Mode {
+  Dark
+  Light
+  Semi
 }
 
 view Button {
-  __display: inlineFlex
-  __MozAppearance: none
-  __appearance: none
+  backgroundColor: red
+  paddingLeft: 10
   __border: 0
-  __cursor: pointer
-  __WebkitAppearance: none
-  __transition: raw("200ms all ease-in-out")
-  width: percentage(100)
-  alignSelf: flexStart
-  flexShrink: 1
-  flexDirection: row
-  paddingTop: 8
-  paddingBottom: 8
-  paddingLeft: 20
-  paddingRight: 20
-  alignItems: center
-  justifyContent: center
-  borderRadius: 7
-  [@disabled] {
-    __boxShadow: none
-    __cursor: notAllowed
-    backgroundColor: rgb(190 190 190)
+  borderWidth: $borderWidth
+  [Type=Primary] {
+    backgroundColor: red
+    paddingLeft: $paddingLeft
+
     [@hover] {
-      __boxShadow: none
-      backgroundColor: rgb(190 190 190)
+      paddingLeft: 10
+      paddingRight: $paddingRight
+    }
+    [Mode=Dark] {
+      backgroundColor: blue
+      paddingLeft: $padLeft
     }
   }
-  [Variant=Primary] {
-    __boxShadow: raw("0 4px 12px rgba(0, 207, 192, 0.4)")
-    backgroundColor: rgb(0 176 164)
-    [@hover] {
-      __boxShadow: raw("0 6px 20px rgba(0, 207, 192, 0.2)")
-      backgroundColor: rgb(0 166 154)
-    }
-    [@active] {
-      backgroundColor: rgb(0 156 144)
-    }
-  }
-  [Variant=Destructive] {
-    __boxShadow: raw("0 4px 12px rgba(220, 60, 110, 0.4)")
-    backgroundColor: rgb(209 50 90)
-    [@hover] {
-      __boxShadow: raw("0 6px 20px rgba(220, 60, 110, 0.2)")
-      backgroundColor: rgb(200 40 80)
-    }
-    [@active] {
-      backgroundColor: rgb(190 30 70)
-    }
-  }
-  [Intent=Inline] {
-    __boxShadow: none
-    borderRadius: 0
-    [@hover] {
-      __boxShadow: none
-    }
-  }
-  [Intent=Outline] {
-    __boxShadow: none
-    backgroundColor: raw("transparent")
-    borderWidth: 2
-    borderStyle: solid
-    [Variant=Primary] {
-      borderColor: rgb(0 176 164)
-      [@hover] {
-        borderColor: rgb(0 166 154)
-      }
-      [@active] {
-        borderColor: rgb(0 156 144)
-      }
-    }
-    [Variant=Destructive] {
-      borderColor: rgb(209 50 90)
-      [@hover] {
-        borderColor: rgb(200 40 80)
-      }
-      [@active] {
-        borderColor: rgb(190 30 70)
-      }
-    }
+
+  [Mode=Light] {
+    backgroundColor: green
   }
 }
 
-text ButtonText {
-  fontSize: 18
-  lineHeight: 1.5
-  color: rgb(255 255 255)
-  textAlign: center
-  letterSpacing: -.1
-  textDecorationLine: none
-  [@disabled] {
-    color: rgb(240 240 240)
+text Label {
+  lineHeight: $lineHeight
+
+  [@hover] {
+    color: red
+    fontSize: $fontSize
   }
-  [Size=Small] {
+
+  [Mode=Light] {
     fontSize: 16
   }
-  [Size=Big] {
-    fontSize: 20
-  }
-  [Intent=Outline] {
-    [Variant=Primary] {
-      __textShadow: raw("0 4px 12px rgba(0, 207, 192, 0.4)")
-      color: rgb(0 176 164)
-    }
-    [Variant=Destructive] {
-      __textShadow: raw("0 4px 12px rgba(220, 60, 110, 0.4)")
-      color: rgb(209 50 90)
-    }
+
+  [@viewportWidth=320] {
+    color: green
+    fontSize: $mediaFontSize
     [@hover] {
-      color: white
+      fontSize: $mediaHoverFontSize
+      color: blue
     }
   }
-}
-`
+}`
 
 describe('Compiling to ReasonML', () => {
-  it.only('should return a map of files', () => {
+  it('should return a map of files', () => {
     const { ast } = parse(file)
 
     expect(createGenerator()(ast, 'root.elo')).toMatchSnapshot()
